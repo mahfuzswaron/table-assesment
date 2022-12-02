b2f582c02d065a38ef0136337ba2d5ac174816bd
mahfuz swaron <mahfuzswaron@gmail.com>
2022-12-02 05:00:37 -0500

    header added'

--- src/App.js
+++ src/App.js
@@ -12,23 +12,30 @@ function App() {
     </div>
   }
   return (
-    <div className="px-20 py-10 grid grid-cols-1 gap-20" >
-      <TableComponent data={data} config={{
-        "columns": ["name", "city", "email", "joiningDate", "role"],
-        "sortingCols": ["name", "city", "email", "joiningDate", "role"]
-      }} />
-      <TableComponent data={data} config={{
-        "columns": ["name", "email", "role"],
-        "sortingCols": ["name"]
-      }} />
-      <TableComponent data={data} config={{
-        "columns": ["email", "joiningDate", "role"],
-        "sortingCols": ["joiningDate", "role"]
-      }} />
-      <TableComponent data={data} config={{
-        "columns": ["name", "city", "joiningDate", "role"],
-        "sortingCols": ["city", "role"]
-      }} />
+    <div className="px-20 py-10">
+      <div className="py-10" >
+        <h1 className="text-5xl text-blue-500 font-medium f-poppins" >Table Assessment Solution</h1>
+        <p className="text-md text-gray-400 my-1 f-poppins">Builed with React.js and Tailwindcss</p>
+
+      </div>
+      <div className="my-10 grid grid-cols-1 gap-12" >
+        <TableComponent data={data} config={{
+          "columns": ["name", "city", "email", "joiningDate", "role"],
+          "sortingCols": ["name", "city", "email", "joiningDate", "role"]
+        }} />
+        <TableComponent data={data} config={{
+          "columns": ["name", "email", "role"],
+          "sortingCols": ["name"]
+        }} />
+        <TableComponent data={data} config={{
+          "columns": ["email", "joiningDate", "role"],
+          "sortingCols": ["joiningDate", "role"]
+        }} />
+        <TableComponent data={data} config={{
+          "columns": ["name", "city", "joiningDate", "role"],
+          "sortingCols": ["city", "role"]
+        }} />
+      </div>
     </div>
   );
 }
