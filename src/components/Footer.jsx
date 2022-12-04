19897b3a7ff0e6b18d503affa9282d77286d3aed
mahfuz swaron <mahfuzswaron@gmail.com>
2022-12-03 23:28:38 -0500

    header and footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className='bg-gray-400 text-center f-poppins mt-5 py-8'>
+        <footer className='bg-[#F5F5F5] text-center f-poppins mt-20 py-5 '>
             <p> &#169; All rights reserved by Mahfuz Swaron</p>
         </footer>
     );
